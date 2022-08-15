3d6efe0c10f327c1c6d79f95fe10ce897c67be7d
Javier Ochoa <jochoaalv@gmail.com>
2022-08-15 18:11:12 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -8,23 +8,23 @@ server.use(express.json())
 
 //if reload is false, it will not reload the database
 //if reload is true, it will reload the database
-let reload = false;
+let reload = true;
 
 db.sequelize.sync({ force: reload }).then(() => {
     server.listen(process.env.PORT, async () => {
         console.log(`Listening in port ${process.env.PORT}`)
 
-        if(reload){
-            console.log('Creating DB...')
-            await axios.get(`http://localhost:3001/charge/one`)
-            await axios.get(`http://localhost:3001/charge/two`)
-            console.log('Creating users for artists...')
-            await axios.get(`http://localhost:3001/asuser`)
-            console.log('Linking albums to main artist...')
-            await axios.get(`http://localhost:3001/albumPatcher`)
-            console.log('patching artist description...')
-            await axios.get(`http://localhost:3001/descriptionPatcher`)
-        }
+//         if(reload){
+//             console.log('Creating DB...')
+//             await axios.get(`http://localhost:3001/charge/one`)
+//             await axios.get(`http://localhost:3001/charge/two`)
+//             console.log('Creating users for artists...')
+//             await axios.get(`http://localhost:3001/asuser`)
+//             console.log('Linking albums to main artist...')
+//             await axios.get(`http://localhost:3001/albumPatcher`)
+//             console.log('patching artist description...')
+//             await axios.get(`http://localhost:3001/descriptionPatcher`)
+//         }
         console.log('DB created')
     })
 });
